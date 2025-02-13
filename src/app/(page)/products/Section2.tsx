2bd9b46eb5ff0c9d4cfebf95efa47451ccbcd64e
Bui Anh Dung <buianh09dung@gmail.com>
2025-02-13 23:56:41 +0700

    ....

--- src/app/(page)/products/Section2.tsx
+++ src/app/(page)/products/Section2.tsx
@@ -1,5 +1,6 @@
 import Aside from "@/app/components/Aside/Aside"
 import CardItem from "@/app/components/Card/CardItem"
+import Pagination from "@/app/components/Pagination/Pagination"
 import { TbPlayerTrackNextFilled } from "react-icons/tb"
 
 const data: any = [
@@ -202,15 +203,7 @@ export default function Section2() {
                                 />
                             ))}
                         </div>
-                        <div className="text-center mt-[50px] mb-[-25px]">
-                            <ul className="flex items-center justify-center">
-                                <li className=" mx-[3px] rounded-[5px] text-[14px] font-[400] text-white bg-primary w-[30px] h-[30px] flex items-center justify-center">1</li>
-                                <li className="mx-[3px] rounded-[5px] text-[14px] font-[400] text-primary bg-white w-[30px] border border-solid border-primary h-[30px] flex items-center justify-center hover:text-white hover:bg-primary">2</li>
-                                <li className="mx-[3px] rounded-[5px] text-[10px] font-[400] text-primary bg-white w-[30px] border border-solid border-primary h-[30px] flex items-center justify-center hover:text-white hover:bg-primary">
-                                    <TbPlayerTrackNextFilled />
-                                </li>
-                            </ul>
-                        </div>
+                        <Pagination/>
                     </div>
                 </div>
             </div>
