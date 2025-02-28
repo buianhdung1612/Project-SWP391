1737934a6098cad74680b3f080280202a7d8857f
Bui Anh Dung <buianh09dung@gmail.com>
2025-02-28 17:58:02 +0700

    ...

--- TinyEditor.tsx
+++ TinyEditor.tsx
@@ -9,18 +9,38 @@ const TinyEditor: React.FC<TinyEditorProps> = ({ value, onEditorChange }) => {
     return (
         <div>
             <Editor
-                apiKey='cjlgkpm42v7mu4b6biit5pt93hgok2oiqf6p2slv9n95xdc1...'
+                apiKey='cjlgkpm42v7mu4b6biit5pt93hgok2oiqf6p2slv9n95xdc1' // Thay thế bằng API key của bạn
                 value={value}
                 init={{
                     height: 350,
-                    plugins: 'autolink lists link image',
-                    toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | bullist numlist outdent indent | link image',
-                    menubar: false,
+                    plugins: [
+                        'a11ychecker', 'accordion', 'advlist', 'anchor', 'autolink', 'autosave',
+                        'charmap', 'code', 'codesample', 'directionality', 'emoticons', 'exportpdf',
+                        'exportword', 'fullscreen', 'help', 'image', 'importcss', 'importword',
+                        'insertdatetime', 'link', 'lists', 'markdown', 'math', 'media', 'nonbreaking',
+                        'pagebreak', 'preview', 'quickbars', 'save', 'searchreplace', 'table',
+                        'visualblocks', 'visualchars', 'wordcount'
+                    ],
+                    toolbar:
+                        'undo redo | accordion accordionremove | \
+                      importword exportword exportpdf | math | \
+                      blocks fontfamily fontsize | bold italic underline strikethrough | \
+                      align numlist bullist | link image | table media | \
+                      lineheight outdent indent | forecolor backcolor removeformat | \
+                      charmap emoticons | code fullscreen preview | save print | \
+                      pagebreak anchor codesample | ltr rtl',
+                    menubar: 'file edit view insert format tools table help',
                     content_style: `
                         body {
-                            font-size: 14px
+                            font-size: 16px; /* Thay đổi font-size mặc định */
                         }
-                    `
+                    `,
+                    font_size_formats: '8pt 10pt 12pt 14pt 18pt 24pt 36pt',
+                    font_formats: 'Arial=arial,helvetica,sans-serif;' +
+                        'Georgia=georgia,palatino;' +
+                        'Tahoma=tahoma,arial,helvetica,sans-serif;' +
+                        'Times New Roman=times new roman,times;' +
+                        'Verdana=verdana,geneva;',
                 }}
                 onEditorChange={onEditorChange}
             />
--- src/app/(page)/blogs/detail/[slug]/page.tsx
+++ src/app/(page)/blogs/detail/[slug]/page.tsx
@@ -1,27 +1,41 @@
-// "use client"
+"use client"
 
-// import { useParams } from "next/navigation";
-// import { useEffect, useState } from "react"
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react"
 
 export default function BlogDetail() {
-    // const { slug } = useParams();
+    const { slug } = useParams();
 
-    // const [data, setData] = useState([]);
+    const [data, setData] = useState({
+        title: "",
+        content: "",
+        author: "",
+        createdAt: ""
+    });
+    const [isLoading, setIsLoading] = useState(true);
 
-    // useEffect(() => {
-    //     const fetchBlogDetail = async () => {
-    //         const response = await fetch();
-    //         const data = await response.json();
-    //     };
+    useEffect(() => {
+        const fetchBlogDetail = async () => {
+            const response = await fetch(`https://freshskinweb.onrender.com/home/blogs/${slug}`);
+            const data = await response.json();
+            setData(data.data);
+            setIsLoading(false);
+        };
 
-    //     fetchBlogDetail();
-    // }, [])
+        fetchBlogDetail();
+    }, [])
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <>
-            <div className="container mx-auto">
-                <div className="text-[26px] font-[650] mb-[15px] text-[#333]">
-                    Serum là gì? Cách sử dụng serum để đạt kết quả tốt nhất
+            <div className="container mx-auto px-[10%]">
+                <div className="text-[24px] font-[650] my-[15px] text-[#333] text-center">
+                    {data.title}
                 </div>
+                <div className="mt-[80px]" dangerouslySetInnerHTML={{ __html: data.content }}></div>
             </div>
         </>
     )
