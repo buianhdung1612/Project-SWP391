7269e6d515e430ad1c853edc6832986bbceb3fd1
Bui Anh Dung <buianh09dung@gmail.com>
2025-03-08 14:47:39 +0700

    ...

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -3,7 +3,7 @@
 import { Box, Typography, TextField, Select, MenuItem, InputLabel, FormControl, Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Checkbox, Chip, Tooltip, Stack, Pagination } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { BiDetail } from "react-icons/bi";
-import { MdDeleteOutline, MdEditNote } from "react-icons/md";
+import { MdDeleteOutline } from "react-icons/md";
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
