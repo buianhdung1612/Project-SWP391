2d470a8a4e78092bf3ace52320fe0e014dd39ae9
Bui Anh Dung <buianh09dung@gmail.com>
2025-03-12 01:28:49 +0700

    ...

--- src/app/admin/quiz/page.tsx
+++ src/app/admin/quiz/page.tsx
@@ -1,10 +1,10 @@
-"use client"
+// "use client"
 
-import { Box, Typography, Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip } from "@mui/material";
-import { MdDeleteOutline, MdEditNote } from "react-icons/md";
+// import { Box, Typography, Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip } from "@mui/material";
+// import { MdDeleteOutline, MdEditNote } from "react-icons/md";
 
-import { useEffect, useState } from "react";
-import Link from "next/link";
+// import { useEffect, useState } from "react";
+// import Link from "next/link";
 
 export default function QuizAdminPage() {
     // const [data, setData] = useState([]);
@@ -43,39 +43,40 @@ export default function QuizAdminPage() {
     // // Hết Xóa một sản phẩm
 
     return (
-        <Box p={3}>
-            {/* Header */}
-            <Typography variant="h5" gutterBottom>
-                Trang bộ đề câu hỏi
-            </Typography>
+        // <Box p={3}>
+        //     {/* Header */}
+        //     <Typography variant="h5" gutterBottom>
+        //         Trang bộ đề câu hỏi
+        //     </Typography>
 
-            {/* Table */}
-            <Paper sx={{ backgroundColor: "white", p: 2 }}>
-                <Box display="flex" gap={20} flexWrap="wrap">
-                    <Button
-                        variant="outlined"
-                        color="success"
-                        sx={{ borderColor: 'green', color: 'green', marginLeft: "88%" }}
-                    >
-                        <Link href="/admin/quiz/create">
-                            + Thêm mới
-                        </Link>
-                    </Button>
-                </Box>
-                <TableContainer sx={{ marginTop: "40px" }} component={Paper}>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>STT</TableCell>
-                                <TableCell>Tiêu đề</TableCell>
-                                {/* <TableCell>Tạo bởi</TableCell> */}
-                                {/* <TableCell>Cập nhật bởi</TableCell> */}
-                                <TableCell>Hành động</TableCell>
-                            </TableRow>
-                        </TableHead>
-                    </Table>
-                </TableContainer>
-            </Paper>
-        </Box>
+        //     {/* Table */}
+        //     <Paper sx={{ backgroundColor: "white", p: 2 }}>
+        //         <Box display="flex" gap={20} flexWrap="wrap">
+        //             <Button
+        //                 variant="outlined"
+        //                 color="success"
+        //                 sx={{ borderColor: 'green', color: 'green', marginLeft: "88%" }}
+        //             >
+        //                 <Link href="/admin/quiz/create">
+        //                     + Thêm mới
+        //                 </Link>
+        //             </Button>
+        //         </Box>
+        //         <TableContainer sx={{ marginTop: "40px" }} component={Paper}>
+        //             <Table>
+        //                 <TableHead>
+        //                     <TableRow>
+        //                         <TableCell>STT</TableCell>
+        //                         <TableCell>Tiêu đề</TableCell>
+        //                         {/* <TableCell>Tạo bởi</TableCell> */}
+        //                         {/* <TableCell>Cập nhật bởi</TableCell> */}
+        //                         <TableCell>Hành động</TableCell>
+        //                     </TableRow>
+        //                 </TableHead>
+        //             </Table>
+        //         </TableContainer>
+        //     </Paper>
+        // </Box>
+        <>dsa</>
     );
 }
