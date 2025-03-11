5ff5fb359aae23f8c12bd5afd4c6c1c0b923da40
Bui Anh Dung <buianh09dung@gmail.com>
2025-03-11 14:40:58 +0700

    ...

--- src/app/(page)/detail/[slug]/Section1.tsx
+++ src/app/(page)/detail/[slug]/Section1.tsx
@@ -8,8 +8,6 @@ import { Context } from "./MiddlewareGetData";
 export default function Section1() {
     const { productDetail } = useContext(Context);
 
-    console.log(productDetail);
-
     return (
         <>
             <ul className="flex items-center mt-[17px] container mx-auto px-3">
@@ -22,14 +20,14 @@ export default function Section1() {
                 {productDetail.category.length > 0 && productDetail.category[0].parent && productDetail.category[0]?.parent?.slug !== "" && (
                     <li>
                         <Link href={`/product-category/${productDetail.category[0].parent.slug}`} className="flex items-center">
-                            <span className="text-[#333] text-[15px] font-[400] hover:text-secondary">{productDetail.category[0].parent.title}</span>
+                            <span className="text-[#333] text-[15px] font-[400] hover:text-secondary">{productDetail.category[0]?.parent?.title}</span>
                             <span><MdNavigateNext className="ml-[10px] text-[18px] mr-[10px]" /></span>
                         </Link>
                     </li>
                 )}
                 <li>
-                    <Link href={`/product-category/${productDetail.category[0].slug}`} className="flex items-center">
-                        <span className="text-[#333] text-[15px] font-[400] hover:text-secondary">{productDetail.category[0].title}</span>
+                    <Link href={`/product-category/${productDetail.category[0]?.slug}`} className="flex items-center">
+                        <span className="text-[#333] text-[15px] font-[400] hover:text-secondary">{productDetail.category[0]?.title}</span>
                         <span><MdNavigateNext className="ml-[10px] text-[18px] mr-[10px]" /></span>
                     </Link>
                 </li>
