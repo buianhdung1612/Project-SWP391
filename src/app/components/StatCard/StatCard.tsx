63ea97478c0db9f0fd7b7ecd0aec691f9aa5120d
minh huy <nguyenminhhuy241204@gmail.com>
2025-03-11 19:39:36 +0700

    nhieu

---
+++ src/app/components/StatCard/StatCard.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+
+interface StatCardProps {
+  value: string;
+  label: string;
+  icon: ReactNode;
+}
+export function StatCard({ value, label, icon }: StatCardProps) {
+  return (
+    <div className="flex items-center bg-white shadow-md rounded-lg p-4 w-52">
+      <div className="text-2xl text-[#98FF98] font-bold">{value}</div>
+      <div className="ml-2">
+        <div className="text-gray-500 text-sm">{label}</div>
+      </div>
+      <div className="ml-auto text-gray-400 text-2xl">{icon}</div>
+    </div>
+  );
+}
