7592714047cdf746963145ee2102bacb34a125e8
Bui Anh Dung <buianh09dung@gmail.com>
2025-03-13 22:37:18 +0700

    Profile Admin

--- src/app/components/header/HeaderAdmin.tsx
+++ src/app/components/header/HeaderAdmin.tsx
@@ -2,6 +2,9 @@ import { FaBell } from "react-icons/fa";
 import { IoSearch } from "react-icons/io5";
 import { FaArrowRightFromBracket } from "react-icons/fa6";
 import Cookies from 'js-cookie';
+import { useContext } from "react";
+import { ProfileAdminContext } from "@/app/admin/layout";
+import Link from "next/link";
 
 export default function HeaderAdmin() {
   const handleClickLogout = async () => {
@@ -9,9 +12,10 @@ export default function HeaderAdmin() {
     location.href = "/admin/auth/login"
   };
 
+  const dataProfile = useContext(ProfileAdminContext);
+
   return (
     <div className="flex items-center justify-between bg-white  p-4  w-full ml-100">
-      {/* Search Bar */}
       <div className="relative w-80">
         <input
           type="text"
@@ -20,17 +24,13 @@ export default function HeaderAdmin() {
         />
         <IoSearch className="absolute left-3 top-3 text-gray-500" size={18} />
       </div>
-      
-      {/* Notification, Language, User Profile, Logout */}
       <div className="flex items-center space-x-6">
-        {/* Notification */}
-        <div className="relative cursor-pointer">
-          <FaBell size={20} className="text-gray-600" />
-          <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">6</span>
-        </div>
+        {dataProfile?.avatar && (
+          <Link href="/admin/profile" className="w-[40px] aspect-square ">
+            <img src={dataProfile?.avatar} className="w-full h-full object-cover rounded-full" />
+          </Link>
+        )}
 
-
-        {/* Logout */}
         <span onClick={handleClickLogout} className="cursor-pointer">
           <FaArrowRightFromBracket className="text-[#6D7587] text-[20px]" />
         </span>
